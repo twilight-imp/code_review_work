695b990d4dd248aaa3e3c1a521acb49119f2fba4
Iratsareva <irinatsarea1211@gmail.com>
2024-12-04 17:53:33 +0300

    update Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,21 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a+b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        return a/b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a*b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver(int a, int b) {
+        int sum = add(a, b);
+        int dif = dif(a, b);
+        int div = div(sum, dif);
+        int times = times(dif, times(a,b));
+        return div;
     }
 }
