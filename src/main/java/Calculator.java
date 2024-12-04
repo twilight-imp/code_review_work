23672a760d703299f68ffab665c497275107043d
Iratsareva <irinatsarea1211@gmail.com>
2024-12-04 20:06:19 +0300

    update Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,16 +6,15 @@ public class Calculator {
         return a-b;
     }
     public int div(int a, int b){
+        if (b == 0) {
+            throw new IllegalArgumentException("Дление на 0");
+        }
         return a/b;
     }
     public int times(int a, int b){
         return a*b;
     }
     public int solver(int a, int b) {
-        int sum = add(a, b);
-        int dif = dif(a, b);
-        int div = div(sum, dif);
-        int times = times(dif, times(a,b));
-        return div;
+        return add(dif(b, a),times(a, b));
     }
 }
